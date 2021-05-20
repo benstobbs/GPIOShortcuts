25cb1bab0ddf509cec52093ffb95ede81d0ca6a8
Ben Stobbs <ben@stobbs.com>
2021-05-20 14:30:17 +0100

    added service and install script

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+if [[ "$EUID" = 0 ]]; then
+    echo "Already root"
+else
+    sudo -k
+    if sudo true; then
+        echo "Correct password"
+    else
+        echo "Wrong password"
+        exit 1
+    fi
+fi
+
+sudo mkdir /etc/GPIOShortcuts
+sudo cp config.txt /etc/GPIOShortcuts/
+sudo cp service.py /etc/GPIOShortcuts/
+sudo cp GPIOShortcuts.service /lib/systemd/system/
+sudo chmod 644 /lib/systemd/system/GPIOShortcuts.service
+sudo chmod +x /etc/GPIOShortcuts/service.py
+
+sudo systemctl daemon-reload
+sudo systemctl enable GPIOShortcuts.service
+sudo systemctl start GPIOShortcuts.service
